7ec6b1a3605c2b5282a31585c0409295387a6fc3
Harshita <chandaliyaharshita222@gmail.com>
2023-07-14 19:13:49 +0530

    Check user's valid first name

---
+++ User-Registration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem!");
+            Console.WriteLine("Valid User First Name!");
+            string name = "^[A-Za-z]{3,}$";
+
+            Regex regex = new Regex(name);
+            Console.WriteLine("Enter first name:");
+            string s = Console.ReadLine();
+            Console.WriteLine("It is: " + regex.IsMatch(s));
+        }
+    }
+}
