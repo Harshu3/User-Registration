69cdf57ba57d7ca6a11231183c9928d932cfbd11
Harshita <chandaliyaharshita222@gmail.com>
2023-07-16 19:21:02 +0530

    Check user's valid email id

--- User-Registration/Program.cs
+++ User-Registration/Program.cs
@@ -8,11 +8,11 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem!");
-            Console.WriteLine("Valid User Last Name!");
-            string name = "^[A-Za-z]{3,}[ ][A-Za-z]{3,}$";
+            Console.WriteLine("Valid User Email ID!");
+            string name = @"[a-z]{3,}.?[a-z0-9]{3,}[@][a-z]{2,}.(com|org)$";
 
             Regex regex = new Regex(name);
-            Console.WriteLine("Enter first and last name:");
+            Console.WriteLine("Enter Email ID:");
             string s = Console.ReadLine();
             Console.WriteLine("It is: " + regex.IsMatch(s));
         }
